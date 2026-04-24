e9b335c1079fb4077858352942b338e94641a71b
OluRemiFour <lekanayoola04@gmail.com>
2026-04-24 20:09:26 +0100

    Implement Theme Toggle (Dark/Light Mode) with next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from "next/app";
 import { Toaster } from 'sonner';
+import { ThemeProvider as NextThemeProvider } from 'next-themes';
 
 import "@/styles/globals.css";
 import { ErrorBoundary } from "@/components/ErrorBoundary";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { ThemeProvider as AppThemeProvider } from "@/contexts/ThemeContext";
 import { WalletProvider } from "@/contexts/WalletContext";
 import ThemeToggle from "@/components/ThemeToggle";
 
@@ -17,18 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <ErrorBoundary>
-      <ThemeProvider>
-        <WalletProvider>
-          <Component {...pageProps} />
-          <ThemeToggle />
-          <Toaster
-            position="top-right"
-            richColors
-            closeButton
-            duration={4000}
-          />
-        </WalletProvider>
-      </ThemeProvider>
+      <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <AppThemeProvider>
+          <WalletProvider>
+            <Component {...pageProps} />
+            <ThemeToggle />
+            <Toaster
+              position="top-right"
+              richColors
+              closeButton
+              duration={4000}
+            />
+          </WalletProvider>
+        </AppThemeProvider>
+      </NextThemeProvider>
     </ErrorBoundary>
   );
 }
